191c614480a570ede37422ae64f6d72b2d439710
fernando luckesi <fernando.luckesi94@gmail.com>
2022-09-21 20:26:48 -0300

    Challenge finished

---
+++ testearray.js
@@ -0,0 +1,28 @@
+const array = [
+  {
+    id: 1,
+      title: 'nada demais',
+      done: false,
+  },
+  {
+    id: 2,
+      title: 'nadar',
+      done: false,
+  },
+  {
+    id: 3,
+      title: 'correr',
+      done: false,
+  }
+];
+
+const id = 2;
+
+const foundItem = array.find(item => item.id === id);
+
+foundItem.done = !foundItem.done
+
+console.log(foundItem)
+
+const newArrayFilterId = array.map(item => ({...item}))
+console.log(newArrayFilterId)
